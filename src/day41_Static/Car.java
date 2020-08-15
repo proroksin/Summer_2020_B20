f0f7b27561df901a452a178237a02c9d267ad083
proroksin <sinaverkemalov@gmail.com>
2020-08-15 11:51:23 -0500

    commit

---
+++ src/day41_Static/Car.java
@@ -0,0 +1,17 @@
+package day41_Static;
+
+public class Car {
+    String brand;
+    String model;
+    int year;
+    double price;
+    String color;
+
+    public void setCarInfo(String model, String brand, int year, double price, String color){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+    }
+}
