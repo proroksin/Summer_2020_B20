fead65a0e8e6a87fee396adf48202934eb1ff702
proroksin <sinaverkemalov@gmail.com>
2020-08-16 09:00:34 -0500

    commit

---
+++ src/day41_toString/Tester.java
@@ -0,0 +1,38 @@
+package day41_toString;
+
+class TesterObject {
+    public static void main(String[] args) {
+        Tester[] testers = {new Tester(), new Tester(), new Tester(), new Tester(), new Tester()};
+
+        testers[0].setInfo("Aalia", 150000, "Female", 18);
+        testers[1].setInfo("Naz", 150000, "Female", 29);
+        testers[2].setInfo("Kalbinur", 155000, "Female", 27);
+        testers[3].setInfo("Rakhat", 130000, "Female", 30);
+        testers[4].setInfo("Edward", 110000, "Male", 36);
+
+        for (Tester each : testers) {
+            System.out.println(each);
+        }
+    }
+}
+
+public class Tester {
+    String name;
+    double salary;
+    String gender;
+    int age;
+
+
+    public void setInfo(String name, double salary, String gender, int age) {
+        this.name = name;
+        this.salary = salary;
+        this.gender = gender;
+        this.age = age;
+    }
+
+
+    public String toString() {
+        return "Name: " + name + ", Salary: " + salary + ", Gender: " + gender + ", Age: " + age;
+    }
+
+}
