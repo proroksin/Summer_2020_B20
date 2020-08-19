ae5f9b503984c87344e9c95834b233597e85c96c
proroksin <sinaverkemalov@gmail.com>
2020-08-19 18:51:21 -0500

    commit

--- src/day44_Constructor/HumanResource.java
+++ src/day44_Constructor/HumanResource.java
@@ -8,7 +8,7 @@ Create a class called HumanResources
  */
 public class HumanResource {
 
-    static Employee employee1;
+    static Employee employee1; // we can call it from the class name
     static Employee employee2;
     static Employee employee3;
     static Employee employee4;
@@ -17,18 +17,18 @@ public class HumanResource {
     //employee1 = new Employee();
     //employee1.setEmployeeInfo();
 
-    static{
+    static{ // as soon as the class called, the static variables need to be set
         employee1= new Employee();
         employee2= new Employee();
         employee3= new Employee();
         employee4= new Employee();
         employee5= new Employee();
 
-        employee1.setEmployeeInfo("Muhammed",12312,456432,"SDET",120000,'M');
-        employee2.setEmployeeInfo("Erkan",45678,123987,"QA",115000,'M');
-        employee3.setEmployeeInfo("Asiya",56431,657098,"Instructor",99000,'F');
-        employee4.setEmployeeInfo("Ayse",17876,342567,"SDET",125000,'F');
-        employee5.setEmployeeInfo("Ahmet",67543,987654,"Developer",140000,'M');
+        employee1.setEmployeeInfo("Muhammed",12312,456432894,"SDET",120000,'M');
+        employee2.setEmployeeInfo("Erkan",45678,123987652,"QA",115000,'M');
+        employee3.setEmployeeInfo("Asiya",56431,657098364,"Instructor",99000,'F');
+        employee4.setEmployeeInfo("Ayse",17876,342567215,"SDET",125000,'F');
+        employee5.setEmployeeInfo("Ahmet",67543,987654784,"Developer",140000,'M');
 
     }
 
