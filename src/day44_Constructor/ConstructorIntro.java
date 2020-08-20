c80d311721c290c689222685cb6942ed159abc71
proroksin <sinaverkemalov@gmail.com>
2020-08-19 20:40:49 -0500

    commit

--- src/day44_Constructor/ConstructorIntro.java
+++ src/day44_Constructor/ConstructorIntro.java
@@ -1,4 +1,28 @@
 package day44_Constructor;
+/*
+declear:
 
+		Access-Modifier  className(){
+				statements;
+		}
+ */
 public class ConstructorIntro {
+
+    public ConstructorIntro(String arr){
+        System.out.println("Hello"+arr);
+        System.out.println("How are you "+arr);
+    }
+
+    public static void main(String[] args) {
+
+        //ConstructorIntro obj = new ConstructorIntro(10);
+
+        //ConstructorIntro obj2 = new ConstructorIntro();
+
+        ConstructorIntro obj3 = new ConstructorIntro(" Sam");
+        ConstructorIntro obj4 = new ConstructorIntro(" Son");
+        ConstructorIntro obj5 = new ConstructorIntro(" Sap");
+
+
+    }
 }
