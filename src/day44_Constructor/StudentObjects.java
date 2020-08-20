fa959897575d0fd78505943378b599d0474912c4
proroksin <sinaverkemalov@gmail.com>
2020-08-19 21:00:07 -0500

    commit

---
+++ src/day44_Constructor/StudentObjects.java
@@ -0,0 +1,17 @@
+package day44_Constructor;
+
+public class StudentObjects {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Meerim",20, 'F');
+        Student student2 = new Student("Nickolas", 21, 'M');
+        Student student3 = new Student("Murat", 21, 'M');
+
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
+
+
+    }
+}
