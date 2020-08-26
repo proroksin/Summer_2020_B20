d9f6768559843bb1c9de47d58920647f954254c5
proroksin <sinaverkemalov@gmail.com>
2020-08-26 18:43:18 -0500

    commit

---
+++ src/Office_Hours/Practice_08_26_2020/task.java
@@ -0,0 +1,15 @@
+package Office_Hours.Practice_08_26_2020;
+
+public class task {
+
+    static int a;
+
+    public static void main(String[] args) {
+        a = 1000;
+        System.out.println(a);
+    }
+
+    static {
+        a = 500;
+    }
+}
---
+++ src/day46_ConstructorCalls/ConstructorCalls2.java
@@ -0,0 +1,35 @@
+package day46_ConstructorCalls;
+
+public class ConstructorCalls2 {
+
+    public ConstructorCalls2(){
+        System.out.println("A");
+    }
+
+    public ConstructorCalls2(int a){ // AB
+
+        //ConstructorCalls2();
+        this(); // calling default constructor
+        System.out.println("B"); // B
+
+    }
+
+    public ConstructorCalls2(String str){
+        this(4); // AB
+        System.out.println("C"); // C
+
+    }
+
+
+    public ConstructorCalls2(char a){
+        this();
+        System.out.println("D");
+    }
+
+
+    public static void main(String[] args) {
+
+        new ConstructorCalls2('A');
+    }
+
+}
