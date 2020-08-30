bef58da361310975faf5eee7d76963c080c70955
proroksin <sinaverkemalov@gmail.com>
2020-08-30 15:37:31 -0500

    commit

--- src/day48_Inheritance/DeviceTask/Phone.java
+++ src/day48_Inheritance/DeviceTask/Phone.java
@@ -1,4 +1,28 @@
 package day48_Inheritance.DeviceTask;
+/*
+create a sub class of device called phone:
+  attributes: brand, model, price, MadeIn, screenSize, hasBattery,hasMemory,hasSimCard,OS
+           methods: call, text, setInfo, toString
+           add a constructor that can set the fileds
+ */
+public class Phone extends Device {
 
-public class Phone {
+    public static boolean hasSimCard = true;
+    public String OS;
+
+    public Phone(String brand, String model, double price, boolean hasBattery, boolean hasMemory, String screenSize, String OS){
+        setInfo(brand, model, price, hasBattery, hasMemory, screenSize);
+        this.OS = OS;
+
+    }
+
+    public void call(long phoneNumber){
+        System.out.println("Calling: "+phoneNumber);
+    }
+
+    public void text(long phoneNumber){
+        System.out.println("Texting to: "+phoneNumber);
+    }
+
+
 }
--- src/day48_Inheritance/DeviceTask/TV.java
+++ src/day48_Inheritance/DeviceTask/TV.java
@@ -1,4 +1,33 @@
 package day48_Inheritance.DeviceTask;
+/*
+2. create a sub class of Device called TV:
+                    attributes: brand, model, price, MadeIn, hasBattery, hasMemory, screenSize, RemoteControl
+                    methods: watch, setInfo, toString
+                    add a constructor that can set the fileds
+ */
+public class TV extends Device {
+    /*
+    Inherited:
+        variables: brand, model, price, MadeIn, hasBattery, hasMemory, screenSize
+        methods: setInfo, toString
+     */
+    /*
+    we do not have yet:
+        variables: remoteControl
+        methods:watch
+     */
 
-public class TV {
+    public boolean hasRemoteControl;
+
+    public TV(String brand, String model, double price, String screenSize, boolean hasBattery, boolean hasMemory, boolean remoteControl){
+        setInfo(brand,model,price,hasBattery,hasMemory, screenSize);
+        this.hasRemoteControl = hasRemoteControl;
+    }
+
+    public void watch(){
+        System.out.println("Watching "+brand+" "+model);
+    }
+
+
+
 }
