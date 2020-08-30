7df74ad209bccf8d26cda14ef78dc2dc325b44b4
proroksin <sinaverkemalov@gmail.com>
2020-08-29 19:14:10 -0500

    commit

---
+++ src/day47_Encapsulation/StaticBlock.java
@@ -0,0 +1,24 @@
+package day47_Encapsulation;
+
+import java.util.ArrayList;
+
+public class StaticBlock {
+
+    static String companyName = "Bank of America";
+
+    static ArrayList<Integer> list = new ArrayList<>();
+
+            static{
+                companyName = "Bank of America";
+                list = new ArrayList<>();
+                list.add(10);
+                list.add(20);
+                list.add(30);
+            }
+
+    public static void main(String[] args) {
+        System.out.println(companyName);
+        System.out.println(list);
+    }
+
+}
