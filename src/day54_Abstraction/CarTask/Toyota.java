aa53198712da5978a9e5496d189ffa4986e673c7
proroksin <sinaverkemalov@gmail.com>
2020-09-15 20:42:38 -0500

    commit

---
+++ src/day54_Abstraction/CarTask/Toyota.java
@@ -0,0 +1,9 @@
+package day54_Abstraction.CarTask;
+
+public class Toyota extends Car{
+
+    @Override
+    public void start() {
+        System.out.println("Starting Toyota by: inserting the key");
+    }
+}
