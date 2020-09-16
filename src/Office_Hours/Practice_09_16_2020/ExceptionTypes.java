d253c10c44538cf40f05cfb46c86ffcfc408f30e
proroksin <sinaverkemalov@gmail.com>
2020-09-16 17:15:31 -0500

    commit

--- src/Office_Hours/Practice_09_16_2020/ExceptionTypes.java
+++ src/Office_Hours/Practice_09_16_2020/ExceptionTypes.java
@@ -2,6 +2,7 @@ package Office_Hours.Practice_09_16_2020;
 
 import java.lang.Exception;
 import java.lang.Object;
+import java.util.NoSuchElementException;
 
 public class ExceptionTypes {
 
@@ -13,17 +14,43 @@ public class ExceptionTypes {
         try{
             System.out.println(9/0);
         }catch(RuntimeException e){
-            System.out.println("Airthmetic exception is caught: " +e.getMessage());
+            System.out.println("Exception is caught: " +e.getMessage());
         }
 
         try {
             Thread.sleep(3000);
         }catch (InterruptedException e){
-            System.out.println("catch block handled it");
+            System.out.println("catch block1 handled it");
+        }catch (Exception e){
+            System.out.println("catch block2 handled it");
         }
+        int[]arr = {1,2,3};
+        try{
+            System.out.println(arr[25]);
+        }catch (RuntimeException e){
+            System.out.println("RuntimeException is handled: "+e.getMessage());
+        }finally {
+            System.out.println("Finally completed");
+        }
+
         System.out.println("Test Case 1 Completed ");
 
 
+        try{// A
+            System.out.println(9/0);
+        }catch (ClassCastException e){// B
+            System.out.println("class cast exception is handled ");
+        }catch (NoSuchElementException e){// C
+            System.out.println("No such element exception is handled");
+        }catch (IndexOutOfBoundsException e){// D
+            System.out.println("index out of bound exception is handled");
+        }catch (RuntimeException e) {// Ultimate solution
+            System.out.println("Runtime exception is nahdled");
+        }finally {
+            System.out.println("finally block");
+        }
+
     }
+
 
 }
