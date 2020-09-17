590818b6f8be07d74ff1d0392b27459512dc58f9
proroksin <sinaverkemalov@gmail.com>
2020-09-16 19:37:39 -0500

    commit

--- src/day55_Abstraction/ShapeTask/Cylinder.java
+++ src/day55_Abstraction/ShapeTask/Cylinder.java
@@ -15,19 +15,42 @@ public final class Cylinder extends Shape {
     public double radius;
     public double height;
 
+    public Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+        area = calculateArea();
+        perimeter = calculatePerimeter();
+        volume = calculateVolume();
+    }
 
     @Override
     public double calculateArea() {
-        return 0;
+        return (2*Math.PI*radius*height)+(2*Math.PI*Math.pow(radius,2));
     }
 
     @Override
     public double calculatePerimeter() {
-        return 0;
+        return (2*Math.PI*height)+(4*Math.PI*Math.pow(radius,2));
     }
 
     @Override
     public double calculateVolume() {
-        return 0;
+        return Math.pow(radius,2)*height*Math.PI;
+    }
+
+    static {
+        name = "Cylinder";
+        hasVolume = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" +
+                "radius=" + radius +
+                ", height=" + height +
+                ", area=" + area +
+                ", perimeter=" + perimeter +
+                ", volume=" + volume +
+                '}';
     }
 }
