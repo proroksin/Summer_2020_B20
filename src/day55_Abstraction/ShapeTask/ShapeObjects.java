ab9968b61fc4bd9033a5378f55583bd10d5017be
proroksin <sinaverkemalov@gmail.com>
2020-09-16 20:42:43 -0500

    commit

--- src/day55_Abstraction/ShapeTask/ShapeObjects.java
+++ src/day55_Abstraction/ShapeTask/ShapeObjects.java
@@ -11,15 +11,30 @@ public class ShapeObjects {
         Circle circle2 = new Circle(5.5);
         System.out.println(circle2);
 
-        System.out.println("===========================");
+        System.out.println("===============================================");
 
         Rectangle rectangle1 = new Rectangle(10,5);
         System.out.println(rectangle1);
 
-        System.out.println("============================");
+        System.out.println("===============================================");
 
         Cylinder cylinder1 = new Cylinder(5,10);
         System.out.println(cylinder1);
+
+        System.out.println(circle1.hasVolume);
+
+        System.out.println("===============================================");
+
+        //Circle circle3 = new Circle(-10);
+        //System.out.println(circle3);
+
+        //Rectangle rectangle2 = new Rectangle(-2,0);
+        //System.out.println(rectangle2);
+
+        Cylinder cylinder2 = new Cylinder(0,0);
+        System.out.println(cylinder2);
+
+
 
     }
 }
