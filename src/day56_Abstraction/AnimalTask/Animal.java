eb3765acadfc31ebd7d6fdd66b81d6d09c8080a3
proroksin <sinaverkemalov@gmail.com>
2020-09-19 11:46:37 -0500

    commit

---
+++ src/day56_Abstraction/AnimalTask/Animal.java
@@ -0,0 +1,7 @@
+package day56_Abstraction.AnimalTask;
+
+public abstract class Animal {// only the common features for all animals
+
+    public abstract void eat();
+    public abstract void sleep();
+}
---
+++ src/day56_Abstraction/AnimalTask/Flyable.java
@@ -0,0 +1,7 @@
+package day56_Abstraction.AnimalTask;
+
+public interface Flyable {
+    boolean hasWings = true;
+
+    public abstract void fly();
+}
