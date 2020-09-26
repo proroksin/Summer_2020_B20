32013d6817e4140b2e4e2fa2307fbe6e8fd94cf7
proroksin <sinaverkemalov@gmail.com>
2020-09-26 09:33:21 -0500

    commit

---
+++ src/day58_Polymorphism/AnimalTask/Dog.java
@@ -0,0 +1,23 @@
+package day58_Polymorphism.AnimalTask;
+
+public class Dog extends Animal{
+
+    public String dogName;
+
+    public Dog(String dogName,int age, char gender) {
+        super(age, gender);
+        this.dogName = dogName;
+    }
+
+    public void bark(){
+        System.out.println("Dog "+dogName+" is barking");
+    }
+
+    public String toString() {
+        return "Dog{" +
+                "dogName='" + dogName + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+}
---
+++ src/day58_Polymorphism/AnimalTask/Zoo.java
@@ -0,0 +1,12 @@
+package day58_Polymorphism.AnimalTask;
+
+public class Zoo {
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Winston",4, 'M');
+
+    }
+
+
+
+}
