ecbe33cc50a8f9da996791557cb03d3d26486676
proroksin <sinaverkemalov@gmail.com>
2020-09-30 18:55:57 -0500

    commit

---
+++ src/day59_OOPReview/Encapsulation.java
@@ -0,0 +1,29 @@
+package day59_OOPReview;
+
+import java.time.LocalDate;
+
+public class Encapsulation {
+
+    private int ssn;//12345
+                    //54321
+
+    private final LocalDate Published = LocalDate.of(2020,9,8); // CANNOT BE MODIFIED
+
+
+    public int getSSN() { // read only
+        return ssn;
+    }
+
+    public void setSsn(int ssn){ // modify only
+        this.ssn = ssn;
+    }
+
+    public LocalDate getPublished(){
+        return Published;
+    }
+
+    // public void setPublished(LocalDate Published){ this.Published = Published; }
+
+
+
+}
---
+++ src/day59_OOPReview/Inheritance.java
@@ -0,0 +1,21 @@
+package day59_OOPReview;
+
+class A{
+
+    private int a = 100;
+    int b = 200;
+    protected int c = 300;
+    public int d = 400;
+
+    private void method1(){}
+    void method2(){}
+    protected void method3(){}
+    public void method4(){}
+
+
+
+}
+
+
+public class Inheritance extends A{
+}
