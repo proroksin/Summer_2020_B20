451e38bf54ddb5c3f435f57d2f187abe29381756
proroksin <sinaverkemalov@gmail.com>
2020-10-12 20:00:56 -0500

    commit

--- src/day60_Collection/SetPractice.java
+++ src/day60_Collection/SetPractice.java
@@ -23,6 +23,58 @@ public class SetPractice {
         str2 = str2.replace("[","").replace(", ","").replace("]","");
         System.out.println(str2);
 
+        System.out.println("===========================================================");
+
+        String a1 = "abcabc"; // a b c
+        String a2 = "cab"; // a b c
+
+        HashSet<String> b1 = new HashSet<>();
+        b1.addAll( Arrays.asList(a1.split("")  ) );
+
+        System.out.println(b1);
+
+        HashSet<String> b2 = new HashSet<>();
+        b2.addAll(Arrays.asList(a2.split("")));
+        System.out.println(b2);
+
+        a1 = b1.toString();
+        a2 = b2.toString();
+
+        System.out.println(a1.equals(a2));
+        System.out.println("==========================================================");
+        String x1 = "cacacabb";
+        String x2 = "bbcaaaaa";
+
+        x1 = new TreeSet<>( Arrays.asList( x1.split("") ) ).toString(); // a b c
+        x2 = new TreeSet<>( Arrays.asList( x2.split("") ) ).toString(); // a b c
+
+        System.out.println(x1.equals(x2));
+        System.out.println("==========================================================");
+
+        String[] names = {"Aalia","Mustafa","Elkem","Aalia", "Virginia"};
+
+        LinkedHashSet<String> set1 = new LinkedHashSet<>();
+        set1.addAll(Arrays.asList( names) );
+
+        names = set1.toArray(new String[0]); // converting collection type to array
+
+        System.out.println(  Arrays.toString(names));
+
+        System.out.println("=============================================================");
+        String[] names2 = {"Roman","Abzal","Nurbiye","Roman","Roman"};
+
+        names2 = new LinkedHashSet<>( Arrays.asList(names2) ).toArray(new String[0]);
+
+        System.out.println(Arrays.toString(names2));
+        System.out.println("==============================================================");
+
+        Integer[] numbers = {1,1,1,2,2,2,0,0,0,0,0,0,1,1,1,2,3,4,5,6,7,8,9};
+
+        numbers = new HashSet<>( Arrays.asList(numbers) ).toArray(new Integer[0]);
+
+        System.out.println(Arrays.toString(numbers));
+
+
 
 
     }
