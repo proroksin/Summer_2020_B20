f47c2137a4cbf5533f0992ae1b0513921fdfe082
proroksin <sinaverkemalov@gmail.com>
2020-10-17 10:29:53 -0500

    commit

--- src/day62_Maps/UniqueCharacters.java
+++ src/day62_Maps/UniqueCharacters.java
@@ -1,19 +1,25 @@
 package day62_Maps;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class UniqueCharacters {
 
     public static void main(String[] args) {
-        String str = "abacbdeef";
+        String str = "abacbdeefhijkl";
         Map<String, Integer> map = new LinkedHashMap<>(); // {c=1, d=1, f=1}
 
-        List<String> list = Arrays.asList(str.split("") );
+        List<String> list = Arrays.asList(str.split(""));
 
+        for (String each : list) { // each: abacbdeef
+            if(Collections.frequency(list, each) == 1){
+                map.put(each, 1);
+            }
+        }
 
+        System.out.println(map);
+
+        // verify that J is unique
+        System.out.println(map.containsKey("j"));
     }
 }
 /*
