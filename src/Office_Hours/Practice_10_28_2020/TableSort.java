7829a24ad43ee24aa5402b616a0d01f503203dfa
proroksin <sinaverkemalov@gmail.com>
2020-10-28 17:42:30 -0500

    up

---
+++ src/Office_Hours/Practice_10_28_2020/TableSort.java
@@ -0,0 +1,35 @@
+package Office_Hours.Practice_10_28_2020;
+
+import java.util.Set;
+
+public class TableSort {
+    /*
+    Let's say there is a table on a web page and you sorted it.
+    This table was given to you as a Set of Strings. Verify if the the table was
+    sorted appropriately
+     */
+
+    public static void main(String[] args) {
+        String s = "java";
+        String s2 = "apple";
+        System.out.println(s.compareTo(s2));
+    }
+
+    public static boolean isSorted(Set<String> set){
+
+        return false;
+
+    }
+
+    /*compareTo
+    ex: s.compareTo(s2)
+    0 -> String are equal
+    positive num -> s come before s2 lexicographically
+
+     */
+
+    //s = "java"
+    //s2 = "apple"
+    // s.compareTo(s2)
+
+}
